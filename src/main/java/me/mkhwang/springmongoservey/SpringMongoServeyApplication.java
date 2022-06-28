b5ffccca07486261d0d285bcf612d05b1cc2d562
황명권 <orange2652@gmail.com>
2022-06-28 16:34:59 +0900

    init

---
+++ src/main/java/me/mkhwang/springmongoservey/SpringMongoServeyApplication.java
@@ -0,0 +1,13 @@
+package me.mkhwang.springmongoservey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMongoServeyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMongoServeyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/mkhwang/springmongoservey/SpringMongoServeyApplicationTests.java
@@ -0,0 +1,13 @@
+package me.mkhwang.springmongoservey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMongoServeyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
